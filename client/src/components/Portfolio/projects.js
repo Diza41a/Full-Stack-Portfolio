705367d393b28bd74f62d7c09471ab3ae86f13bb
Davyd Zakorchennyi <Diza41a@mail.com>
2022-09-02 15:18:21 -0400

    Updated project portfolio with some pictures

--- client/src/components/Portfolio/projects.js
+++ client/src/components/Portfolio/projects.js
@@ -1,23 +1,5 @@
 const projects = [
   {
-    title: 'Atelier Clothing Platform',
-    description: 'The opening widget on the application provides detailed '
-    + 'information on the current product displayed. '
-    + 'User is able to select different styles , '
-    + 'colors, sizes and quantities from a selection menu. '
-    + 'The opening widget on the application provides detailed '
-    + 'information on the current product displayed.User is able '
-    + 'to select different styles , colors, sizes and quantities '
-    + 'from a selection menu.',
-    'github-link': 'https://github.com/Diza41a/ofkors-european-bakery',
-    'deployed-link': 'http://ofkorseurobakery.com/',
-    images: {
-      main: ['./assets/images/projects/atelier/test.png', './assets/images/projects/atelier/test.png', './assets/images/projects/atelier/test.png'],
-      mobile: ['./assets/images/projects/atelier/test-mobile.png', './assets/images/projects/atelier/test-mobile.png', './assets/images/projects/atelier/test-mobile.png'],
-    },
-
-  },
-  {
     title: 'OfKors European Bakery',
     description: 'The opening widget on the application provides detailed '
     + 'information on the current product displayed. '
@@ -30,7 +12,40 @@ const projects = [
     'github-link': 'https://github.com/Diza41a/ofkors-european-bakery',
     'deployed-link': 'http://ofkorseurobakery.com/',
     images: {
-      main: ['./assets/images/projects/atelier/test.png', './assets/images/projects/atelier/test.png', './assets/images/projects/atelier/test.png'],
+      main: [
+        './assets/images/projects/bakery/main/1.png',
+        './assets/images/projects/bakery/main/2.png',
+        './assets/images/projects/bakery/main/3.png',
+        './assets/images/projects/bakery/main/4.png',
+        './assets/images/projects/bakery/main/5.png',
+        './assets/images/projects/bakery/main/6.png',
+        './assets/images/projects/bakery/main/7.png',
+      ],
+      mobile: [
+        './assets/images/projects/bakery/mobile/1.png',
+        './assets/images/projects/bakery/mobile/2.png',
+        './assets/images/projects/bakery/mobile/3.png',
+        './assets/images/projects/bakery/mobile/4.png',
+        './assets/images/projects/bakery/mobile/5.png',
+      ],
+    },
+  },
+  {
+    title: 'Atelier Clothing Platform',
+    description: 'The opening widget on the application provides detailed '
+    + 'information on the current product displayed. '
+    + 'User is able to select different styles , '
+    + 'colors, sizes and quantities from a selection menu. '
+    + 'The opening widget on the application provides detailed '
+    + 'information on the current product displayed.User is able '
+    + 'to select different styles , colors, sizes and quantities '
+    + 'from a selection menu.',
+    'github-link': 'https://github.com/Diza41a/ofkors-european-bakery',
+    'deployed-link': 'http://ofkorseurobakery.com/',
+    images: {
+      main: [
+        './assets/images/projects/atelier/test.png', './assets/images/projects/atelier/test.png', './assets/images/projects/atelier/test.png',
+      ],
       mobile: ['./assets/images/projects/atelier/test-mobile.png', './assets/images/projects/atelier/test-mobile.png', './assets/images/projects/atelier/test-mobile.png'],
     },
   },
@@ -64,8 +79,17 @@ const projects = [
     'github-link': 'https://github.com/Diza41a/ofkors-european-bakery',
     'deployed-link': 'http://ofkorseurobakery.com/',
     images: {
-      main: ['./assets/images/projects/atelier/test.png', './assets/images/projects/atelier/test.png', './assets/images/projects/atelier/test.png'],
-      mobile: ['./assets/images/projects/atelier/test-mobile.png', './assets/images/projects/atelier/test-mobile.png', './assets/images/projects/atelier/test-mobile.png'],
+      main: [
+        './assets/images/projects/checkers/main/1.png',
+        './assets/images/projects/checkers/main/2.png',
+        './assets/images/projects/checkers/main/3.png',
+        './assets/images/projects/checkers/main/4.png',
+      ],
+      mobile: [
+        './assets/images/projects/checkers/mobile/1.png',
+        './assets/images/projects/checkers/mobile/2.png',
+        './assets/images/projects/checkers/mobile/3.png',
+      ],
     },
   },
 ];
